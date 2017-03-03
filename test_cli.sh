4a19c9df4a7b5c58eb8eee9bc79be902c6418895
Aleksandr Sidoruk <alex.sidoruk@mail.ru>
2017-03-03 14:37:26 +0300

    Update test_cli.sh

--- test_cli.sh
+++ test_cli.sh
@@ -1,15 +1,13 @@
 echo 'mkdir /ngs && cd /ngs'
 mkdir /ngs && cd /ngs
 
-echo 'TEST: Download distr'
+#echo 'TEST: Download distr'
 wget http://ngb.opensource.epam.com/distr/2.3/catgenome-2.3.jar
 wget http://ngb.opensource.epam.com/distr/2.3/ngb-cli-2.3.tar.gz
 
-echo 'TEST: Download data'
+#echo 'TEST: Download data'
 wget http://ngb.opensource.epam.com/distr/data/genome/dm6/dmel-all-chromosome-r6.06.fasta
 wget http://ngb.opensource.epam.com/distr/data/genome/dm6/dmel-all-chromosome-r6.06.fasta.fai
-
-ls -la
 
 echo 'mv catgenome-2.3.jar catgenome.jar'
 mv catgenome-2.3.jar catgenome.jar
@@ -25,3 +23,12 @@ sleep 30
 
 echo 'cat nohup.out'
 cat nohup.out
+
+echo 'cd /ngs/ngb-cli/bin'
+cd /ngs/ngb-cli/bin
+
+echo './ngb reg_ref /ngs/dmel-all-chromosome-r6.06.fasta --name dm6'
+./ngb reg_reg /ngs/dmel-all-chromosome-r6.06.fasta --name dm6
+
+echo './ngb lr -t'
+./ngb lr -t
