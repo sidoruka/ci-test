86d5404312690fa256a785b73bdb18fc45c17c7c
Aleksandr Sidoruk <alex.sidoruk@mail.ru>
2017-03-03 18:23:03 +0300

    Update cli-integration-tests.sh

--- cli-integration-tests.sh
+++ cli-integration-tests.sh
@@ -1,6 +1,23 @@
+#-------------INIT VARIABLES-------------
+
+# General
+NGS_FOLDER=/ngs/
+CLI_FOLDER=${NGS_FOLDER}ngb-cli/bin
+
+# Reference
+REFERENCE_NAME=ref
+FASTA=dmel-all-chromosome-r6.06.fasta
+
+# Datasets
+
+# Files
+GTF=GTF
+
+#-----------------INIT WORKING DIRECTORY------------------
+
 # Create integration tests working directory
-echo 'mkdir /ngs && cd /ngs'
-mkdir /ngs && cd /ngs
+echo 'mkdir /${NGS_FOLDER} && cd /${NGS_FOLDER}'
+mkdir /${NGS_FOLDER} && cd /${NGS_FOLDER}
 
 echo 'TEST: Download distr'
 wget http://ngb.opensource.epam.com/distr/2.3/catgenome-2.3.jar
@@ -16,6 +33,7 @@ echo 'TEST: Download data'
 wget http://ngb.opensource.epam.com/distr/data/genome/dm6/dmel-all-chromosome-r6.06.fasta
 wget http://ngb.opensource.epam.com/distr/data/genome/dm6/dmel-all-chromosome-r6.06.fasta.fai
 
+#-----------------INIT NGB AND CLI------------------
 # Start NGB as a service
 echo 'nohup java -Xmx3G -jar catgenome.jar &'
 nohup java -Xmx3G -jar catgenome.jar &
@@ -29,10 +47,120 @@ echo 'cat nohup.out'
 cat nohup.out
 
 # Navigate to CLI command location
-echo 'cd /ngs/ngb-cli/bin'
-cd /ngs/ngb-cli/bin
+echo 'cd ${CLI_FOLDER}'
+cd ${CLI_FOLDER}
 
 #-----------------TESTS------------------
+
+# Reference commands
+echo 'Reference commands'
+echo '--------'
+
+## reg_ref
+echo 'Should register reference with default name'
+./ngb reg_ref ${NGS_FOLDER}${FASTA} && echo 'PASSED'
+
+## reg_ref -n
+echo 'Should register reference with explicit name'
+./ngb reg_ref ${NGS_FOLDER}${FASTA} --name ${REFERENCE_NAME} && echo 'PASSED'
+
+## reg_ref -g
+## <TODO>
+
+## list_ref
+## <TODO>
+
+## del_ref
+echo 'Should delete reference with default name'
+./ngb del_ref ${FASTA} && echo 'PASSED'
+
+echo 'Should delete reference with explicit name'
+./ngb del_ref ${REFERENCE_NAME} && echo 'PASSED'
+
+## add_genes
+## <TODO>
+
+## remove_genes
+## <TODO>
+
+# Datasets commands
+
+## reg_dataset
+## <TODO>
+
+## reg_dataset -p
+## <TODO>
+
+## reg_dataset [file-names]
+## <TODO>
+
+## add_dataset
+## <TODO>
+
+## remove_dataset
+## <TODO>
+
+## move_dataset
+## <TODO>
+
+## list_dataset -p
+## <TODO>
+
+## del_dataset
+## <TODO>
+
+## url 
+## <TODO>
+
+## url -loc:chr
+## <TODO>
+
+## url -loc:chr:range
+## <TODO>
+
+## url -[file-names]
+## <TODO>
+
+
+# Files
+## BAM
+
+### reg_file
+## <TODO>
+
+### reg_file -n
+## <TODO>
+
+### reg_file -ni
+## <TODO>
+
+### del_file
+## <TODO>
+
+### index_file
+## <TODO>
+
+## CRAM
+## <TODO>
+
+## GFF
+## <TODO>
+
+## GTF
+## <TODO>
+
+## BED
+## <TODO>
+
+## VCF (SVs, SNPs, InDels)
+## <TODO>
+
+## BW
+## <TODO>
+
+## SEG
+## <TODO>
+
 
 # General commands
 echo 'General commands'
@@ -43,75 +171,7 @@ echo 'Should allow to configure NGB server address'
 ./ngb set_srv http://localhost:8080/catgenome
 
 ## search
+## <TODO>
 
 ## search -l
-
-# Reference commands
-echo 'Reference commands'
-echo '--------'
-
-## reg_ref
-echo 'Should register DM6 reference'
-echo './ngb reg_ref /ngs/dmel-all-chromosome-r6.06.fasta --name dm6'
-./ngb reg_ref /ngs/dmel-all-chromosome-r6.06.fasta --name dm6
-
-echo 'Should register GRCh38 reference'
-
-## reg_ref -n
-
-## reg_ref -g
-
-# Datasets commands
-echo 'Datasets commands'
-echo '--------'
-
-## reg_dataset
-
-## reg_dataset -p
-
-## reg_dataset [file-names]
-
-## add_dataset
-
-## remove_dataset
-
-## move_dataset
-
-## list_dataset -p
-
-## del_dataset
-
-## url 
-
-## url -loc:chr
-
-## url -loc:chr:range
-
-## url -[file-names]
-
-# Files
-## BAM
-
-### reg_file
-
-### reg_file -n
-
-### reg_file -ni
-
-### del_file
-
-### index_file
-
-## CRAM
-
-## GFF
-
-## GTF
-
-## BED
-
-## VCF (SVs, SNPs, InDels)
-
-## BW
-
-## SEG
+## <TODO>
