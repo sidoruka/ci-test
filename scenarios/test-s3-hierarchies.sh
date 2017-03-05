7c2dede1f722ab6d089e13a3c472ebfdc3129829
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2017-03-05 16:34:42 +0300

    no message

--- scenarios/test-s3-hierarchies.sh
+++ scenarios/test-s3-hierarchies.sh
@@ -1,7 +1,7 @@
-DATASETS_COUNT=10
+DATASETS_COUNT=3
 
 echo
-echo Scenario 3. Hierarchies operations. Will create 3-level datasets hierachy with 10 nodes each
+echo Scenario 3. Hierarchies operations. Will create 3-level datasets hierachy with ${DATASETS_COUNT} nodes each
 echo ------------------------------
 
 it \
@@ -12,23 +12,23 @@ it \
     "Should register reference with index and explicit name" \
     "ngb reg_ref ${FASTA} --name ${REFERENCE_NAME}"
 
-for DATASET_NUM in 1 .. $DATASETS_COUNT
+for DATASET_NUM in $(seq 1 $DATASETS_COUNT)
 do
     ROOT_L1_NAME=${DATASET_ROOT}_${DATASET_NUM}
 	it \
         "Should register root dataset ${ROOT_L1_NAME} for reference" \
         "ngb reg_dataset ${REFERENCE_NAME} ${ROOT_L1_NAME}"
 
-        for DATASET_NUM_L2 in 1 .. $DATASETS_COUNT
+        for DATASET_NUM_L2 in $(seq 1 $DATASETS_COUNT)
         do
             CHILD_L2_NAME=${DATASET_CHILD}_${DATASET_NUM_L2}
             it \
                 "Should register child dataset ${CHILD_L2_NAME} for reference" \
                 "ngb reg_dataset ${REFERENCE_NAME} ${CHILD_L2_NAME} -p ${ROOT_L1_NAME}"
 
-                for DATASET_NUM_L3 in 1 .. $DATASETS_COUNT
+                for DATASET_NUM_L3 in $(seq 1 $DATASETS_COUNT)
                 do
-                    CHILD_L3_NAME=${DATASET_CHILD}_${DATASET_NUM_L3}
+                    CHILD_L3_NAME=${DATASET_CHILD}_${DATASET_NUM_L2}_${DATASET_NUM_L3}
                     it \
                         "Should register child dataset ${CHILD_L3_NAME} for reference" \
                         "ngb reg_dataset ${REFERENCE_NAME} ${CHILD_L3_NAME} -p ${CHILD_L2_NAME}"
