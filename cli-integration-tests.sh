184ed79c0f0716fe338ba9609cde89bffc555061
Aleksandr Sidoruk <aleksandr_sidoruk@epam.com>
2017-03-05 14:51:46 +0300

    no message

--- cli-integration-tests.sh
+++ cli-integration-tests.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-function print_success {
+export -f function print_success {
   echo `tput setaf 2`$1`tput sgr0`
 }
 
-function print_error {
+export -f function print_error {
   echo `tput setaf 1`$1`tput sgr0`
 }
 
-function escape_string {
+export -f function escape_string {
     echo $1 | sed 's/\"/\\"/g' | sed 's/\[/\\[/g' | sed 's/\]/\\]/g'
 }
 
-function skip_it {
+export -f function skip_it {
   echo $1
   echo "--> "$2
   ((SKIPPED_COUNT++))
@@ -20,7 +20,7 @@ function skip_it {
   echo
 }
 
-function it {
+export -f function it {
   echo $1
   echo "--> CMD: "$2
   
@@ -56,34 +56,34 @@ function it {
 #-------------INIT VARIABLES-------------
 
 # General
-FAILED_COUNT=0
-PASSED_COUNT=0
-SKIPPED_COUNT=0
-NGS_FOLDER=/ngs/
-CLI_FOLDER=${NGS_FOLDER}ngb-cli/bin
+export FAILED_COUNT=0
+export PASSED_COUNT=0
+export SKIPPED_COUNT=0
+export NGS_FOLDER=/ngs/
+export CLI_FOLDER=${NGS_FOLDER}ngb-cli/bin
 
 # Reference
-REFERENCE_NAME=ref
-FASTA=${NGS_FOLDER}dmel-all-chromosome-r6.06.fasta
-FASTA_INDEX=${NGS_FOLDER}dmel-all-chromosome-r6.06.fasta.fai
-FASTA_NO_EXT="$(basename "$FASTA" | sed -e 's/\.[^.]*$//')"
-CHR=X
+export REFERENCE_NAME=ref
+export FASTA=${NGS_FOLDER}dmel-all-chromosome-r6.06.fasta
+export FASTA_INDEX=${NGS_FOLDER}dmel-all-chromosome-r6.06.fasta.fai
+export FASTA_NO_EXT="$(basename "$FASTA" | sed -e 's/\.[^.]*$//')"
+export CHR=X
 
 # Datasets
-DATASET_ROOT=root_dataset
-DATASET_CHILD=child_dataset
+export DATASET_ROOT=root_dataset
+export DATASET_CHILD=child_dataset
 
 # Files
-GTF=${NGS_FOLDER}dmel-all-r6.06.sorted.gtf.gz
-GTF_INDEX=${NGS_FOLDER}dmel-all-r6.06.sorted.gtf.gz.tbi
-GTF_NAME=genes_file
+export GTF=${NGS_FOLDER}dmel-all-r6.06.sorted.gtf.gz
+export GTF_INDEX=${NGS_FOLDER}dmel-all-r6.06.sorted.gtf.gz.tbi
+export GTF_NAME=genes_file
 
-BAM=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.bam
-BAM_INDEX=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.bai
-BAM_NAME=bam_file
+export BAM=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.bam
+export BAM_INDEX=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.bai
+export BAM_NAME=bam_file
 
-VCF_SNP=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.vcf
-VCF_SNP_NAME=vcf_file
+export VCF_SNP=${NGS_FOLDER}agnX1.09-28.trim.dm606.realign.vcf
+export VCF_SNP_NAME=vcf_file
 
 #-----------------INIT WORKING DIRECTORY------------------
 
@@ -91,6 +91,7 @@ VCF_SNP_NAME=vcf_file
 echo 'mkdir /${NGS_FOLDER} && cd /${NGS_FOLDER}'
 mkdir /${NGS_FOLDER} && cd /${NGS_FOLDER}
 
+# Temporary staff
 echo 'TEST: Download distr'
 wget http://ngb.opensource.epam.com/distr/2.3/catgenome-2.3.jar
 wget http://ngb.opensource.epam.com/distr/2.3/ngb-cli-2.3.tar.gz
@@ -100,6 +101,8 @@ mv catgenome-2.3.jar catgenome.jar
 # Unzip CLI
 echo 'tar -zxvf ngb-cli-2.3.tar.gz'
 tar -zxvf ngb-cli-2.3.tar.gz
+
+#-----------------DOWNLOAD TEST DATA------------------
 
 echo 'TEST: Download data'
 wget http://ngb.opensource.epam.com/distr/data/genome/dm6/dmel-all-chromosome-r6.06.fasta
@@ -127,213 +130,16 @@ cat nohup.out
 echo 'cd ${CLI_FOLDER}'
 cd ${CLI_FOLDER}
 
-#-----------------TESTS------------------
-echo
-echo Scenario 1. Simple Dataset. Registration phase
-echo ------------------------------
+#-------------------RUN SCENARIOS-------------------
 
-it \
-  "Should configure NGB server address" \
-  "./ngb set_srv http://localhost:8080/catgenome"
+for file in scenarios/*
+do
+    if [[ -f $file ]] %% [[ $file == test-*.sh ]]; then
+        /bin/bash $file
+    fi
+done
 
-it \
-    "Should register reference with index and explicit name" \
-    "./ngb reg_ref ${FASTA} --name ${REFERENCE_NAME}"
-
-it \
-    "Should register gzipped genes file with explicit name" \
-    "./ngb reg_file ${REFERENCE_NAME} ${GTF}?${GTF_INDEX} --name ${GTF_NAME}"
-
-it \
-    "Should assign registered genes file to a reference by name" \
-    "./ngb add_genes ${REFERENCE_NAME} ${GTF_NAME}"
-
-it \
-    "Should register root dataset for reference" \
-    "./ngb reg_dataset ${REFERENCE_NAME} ${DATASET_ROOT}"
-
-it \
-    "Should register BAM file with explicit name" \
-    "./ngb reg_file ${REFERENCE_NAME} ${BAM}?${BAM_INDEX} --name ${BAM_NAME}"
-
-it \
-    "Should add registered BAM file to root dataset" \
-    "./ngb add_dataset ${DATASET_ROOT} ${BAM_NAME}"
-
-it \
-    "Should register VCF file with explicit name" \
-    "./ngb reg_file ${REFERENCE_NAME} ${VCF_SNP} --name ${VCF_SNP_NAME}"
-
-it \
-    "Should add registered VCF file to root dataset" \
-    "./ngb add_dataset ${DATASET_ROOT} ${VCF_SNP_NAME}"
-
-echo
-echo Scenario 1. Simple Dataset. Check phase - search registered items
-echo ------------------------------
-
-it \
-    "Should list registered reference and output in a table format" \
-    "./ngb list_ref -t" \
-    "${FASTA}"
-
-it \
-    "Should find registered reference by strict name and output in a table format" \
-    "./ngb search ${REFERENCE_NAME} -t" \
-    "asdc"
-
-it \
-    "Should list registered dataset and output in a table format" \
-    "./ngb list_dataset -t"
-
-it \
-    "Should find registered GTF file by strict name and output in a table format" \
-    "./ngb search ${GTF_NAME} -t"
-
-it \
-    "Should find registered BAM file by strict name and output in a table format" \
-    "./ngb search ${BAM_NAME} -t"
-
-it \
-    "Should find registered VCF file by strict name and output in a table format" \
-    "./ngb search ${VCF_SNP_NAME} -t"
-    
-it \
-    "Should generate url for whole dataset" \
-    "./ngb url ${DATASET_ROOT}" \
-    "http://localhost:8080/catgenome/#/ref?tracks=\[{\"p\":\"${DATASET_ROOT}\"}\]"
-
-it \
-    "Should generate url for whole root dataset at 1st chr" \
-    "./ngb url ${DATASET_ROOT} --location ${CHR}" \
-    "http://localhost:8080/catgenome/#/ref/X?tracks=\[{\"p\":\"${DATASET_ROOT}\"}\]"
-
-it \
-    "Should generate url for whole root dataset at 1st chr and range 1000bp-2000bp" \
-    "./ngb url ${DATASET_ROOT} --location ${CHR}:1000-2000"
-    
-it \
-    "Should generate url for only one BAM file from a root dataset at 1st chr and range 1000bp-2000bp" \
-    "./ngb url ${DATASET_ROOT} ${BAM_NAME} --location ${CHR}:1000-2000"
-    
-it \
-    "Should generate url for only BAM and VCF files from a root dataset at 1st chr and range 1000bp-2000bp" \
-    "./ngb url ${DATASET_ROOT} ${BAM_NAME} ${VCF_SNP_NAME} --location ${CHR}:1000-2000"
-
-echo
-echo Scenario 1. Simple Dataset. Clean-up phase - delete registered items
-echo ------------------------------
-
-it \
-    "Should remove BAM file from dataset" \
-    "./ngb remove_dataset ${DATASET_ROOT} ${BAM_NAME}"
-
-it \
-    "Should remove VCF file from dataset" \
-    "./ngb remove_dataset ${DATASET_ROOT} ${VCF_SNP_NAME}"
-
-it \
-    "Should delete root dataset" \
-    "./ngb del_dataset ${DATASET_ROOT}"
-
-it \
-    "Should delete BAM file" \
-    "./ngb del_file ${BAM_NAME}"
-
-it \
-    "Should delete VCF file" \
-    "./ngb del_file ${VCF_SNP_NAME}"
-
-it \
-    "Should remove GTF file from reference" \
-    "./ngb remove_genes ${REFERENCE_NAME}"
-
-it \
-    "Should delete GTF file" \
-    "./ngb del_file ${GTF_NAME}"
-
-it \
-    "Should delete reference" \
-    "./ngb del_ref ${REFERENCE_NAME}"
-
-# Reference commands
-
-## reg_ref
-## <TODO>
-
-## reg_ref -g
-## <TODO>
-
-## reg_ref -ngc
-## <TODO>
-
-## add_genes [file-paths]
-## <TODO>
-
-# Datasets commands
-
-## reg_dataset -p
-## <TODO>
-
-## reg_dataset [file-names]
-## <TODO>
-
-## reg_dataset [file-paths]
-## <TODO>
-
-## add_dataset [file-paths]
-## <TODO>
-
-## move_dataset
-## <TODO>
-
-## list_dataset -p
-## <TODO>
-
-# Files
-## BAM
-
-### reg_file
-## <TODO>
-
-### reg_file -n
-## <TODO>
-
-### reg_file -ni
-## <TODO>
-
-### del_file
-## <TODO>
-
-### index_file
-## <TODO>
-
-## CRAM
-## <TODO>
-
-## GFF
-## <TODO>
-
-## GTF
-## <TODO>
-
-## BED
-## <TODO>
-
-## VCF (SVs, SNPs, InDels)
-## <TODO>
-
-## BW
-## <TODO>
-
-## SEG
-## <TODO>
-
-
-# General commands
-
-## search -l
-## <TODO>
+#-------------------PRINT RESULTS-------------------
 
 # Tests results
 echo TOTAL: $((PASSED_COUNT + FAILED_COUNT + SKIPPED_COUNT))
---
+++ scenarios/test-s1-basic.sh
@@ -0,0 +1,210 @@
+echo
+echo Scenario 1. Simple Dataset. Registration phase
+echo ------------------------------
+
+it \
+  "Should configure NGB server address" \
+  "./ngb set_srv http://localhost:8080/catgenome"
+
+it \
+    "Should register reference with index and explicit name" \
+    "./ngb reg_ref ${FASTA} --name ${REFERENCE_NAME}"
+
+it \
+    "Should register gzipped genes file with explicit name" \
+    "./ngb reg_file ${REFERENCE_NAME} ${GTF}?${GTF_INDEX} --name ${GTF_NAME}"
+
+it \
+    "Should assign registered genes file to a reference by name" \
+    "./ngb add_genes ${REFERENCE_NAME} ${GTF_NAME}"
+
+it \
+    "Should register root dataset for reference" \
+    "./ngb reg_dataset ${REFERENCE_NAME} ${DATASET_ROOT}"
+
+it \
+    "Should register BAM file with explicit name" \
+    "./ngb reg_file ${REFERENCE_NAME} ${BAM}?${BAM_INDEX} --name ${BAM_NAME}"
+
+it \
+    "Should add registered BAM file to root dataset" \
+    "./ngb add_dataset ${DATASET_ROOT} ${BAM_NAME}"
+
+it \
+    "Should register VCF file with explicit name" \
+    "./ngb reg_file ${REFERENCE_NAME} ${VCF_SNP} --name ${VCF_SNP_NAME}"
+
+it \
+    "Should add registered VCF file to root dataset" \
+    "./ngb add_dataset ${DATASET_ROOT} ${VCF_SNP_NAME}"
+
+echo
+echo Scenario 1. Simple Dataset. Check phase - search registered items
+echo ------------------------------
+
+it \
+    "Should list registered reference and output in a table format" \
+    "./ngb list_ref -t" \
+    "${FASTA}"
+
+it \
+    "Should find registered reference by strict name and output in a table format" \
+    "./ngb search ${REFERENCE_NAME} -t" \
+    "asdc"
+
+it \
+    "Should list registered dataset and output in a table format" \
+    "./ngb list_dataset -t"
+
+it \
+    "Should find registered GTF file by strict name and output in a table format" \
+    "./ngb search ${GTF_NAME} -t"
+
+it \
+    "Should find registered BAM file by strict name and output in a table format" \
+    "./ngb search ${BAM_NAME} -t"
+
+it \
+    "Should find registered VCF file by strict name and output in a table format" \
+    "./ngb search ${VCF_SNP_NAME} -t"
+    
+it \
+    "Should generate url for whole dataset" \
+    "./ngb url ${DATASET_ROOT}" \
+    "http://localhost:8080/catgenome/#/ref?tracks=\[{\"p\":\"${DATASET_ROOT}\"}\]"
+
+it \
+    "Should generate url for whole root dataset at 1st chr" \
+    "./ngb url ${DATASET_ROOT} --location ${CHR}" \
+    "http://localhost:8080/catgenome/#/ref/X?tracks=\[{\"p\":\"${DATASET_ROOT}\"}\]"
+
+it \
+    "Should generate url for whole root dataset at 1st chr and range 1000bp-2000bp" \
+    "./ngb url ${DATASET_ROOT} --location ${CHR}:1000-2000"
+    
+it \
+    "Should generate url for only one BAM file from a root dataset at 1st chr and range 1000bp-2000bp" \
+    "./ngb url ${DATASET_ROOT} ${BAM_NAME} --location ${CHR}:1000-2000"
+    
+it \
+    "Should generate url for only BAM and VCF files from a root dataset at 1st chr and range 1000bp-2000bp" \
+    "./ngb url ${DATASET_ROOT} ${BAM_NAME} ${VCF_SNP_NAME} --location ${CHR}:1000-2000"
+
+echo
+echo Scenario 1. Simple Dataset. Clean-up phase - delete registered items
+echo ------------------------------
+
+it \
+    "Should remove BAM file from dataset" \
+    "./ngb remove_dataset ${DATASET_ROOT} ${BAM_NAME}"
+
+it \
+    "Should remove VCF file from dataset" \
+    "./ngb remove_dataset ${DATASET_ROOT} ${VCF_SNP_NAME}"
+
+it \
+    "Should delete root dataset" \
+    "./ngb del_dataset ${DATASET_ROOT}"
+
+it \
+    "Should delete BAM file" \
+    "./ngb del_file ${BAM_NAME}"
+
+it \
+    "Should delete VCF file" \
+    "./ngb del_file ${VCF_SNP_NAME}"
+
+it \
+    "Should remove GTF file from reference" \
+    "./ngb remove_genes ${REFERENCE_NAME}"
+
+it \
+    "Should delete GTF file" \
+    "./ngb del_file ${GTF_NAME}"
+
+it \
+    "Should delete reference" \
+    "./ngb del_ref ${REFERENCE_NAME}"
+
+echo ------------------------------
+echo Scenario 1. Finished
+echo
+
+# Reference commands
+
+## reg_ref
+## <TODO>
+
+## reg_ref -g
+## <TODO>
+
+## reg_ref -ngc
+## <TODO>
+
+## add_genes [file-paths]
+## <TODO>
+
+# Datasets commands
+
+## reg_dataset -p
+## <TODO>
+
+## reg_dataset [file-names]
+## <TODO>
+
+## reg_dataset [file-paths]
+## <TODO>
+
+## add_dataset [file-paths]
+## <TODO>
+
+## move_dataset
+## <TODO>
+
+## list_dataset -p
+## <TODO>
+
+# Files
+## BAM
+
+### reg_file
+## <TODO>
+
+### reg_file -n
+## <TODO>
+
+### reg_file -ni
+## <TODO>
+
+### del_file
+## <TODO>
+
+### index_file
+## <TODO>
+
+## CRAM
+## <TODO>
+
+## GFF
+## <TODO>
+
+## GTF
+## <TODO>
+
+## BED
+## <TODO>
+
+## VCF (SVs, SNPs, InDels)
+## <TODO>
+
+## BW
+## <TODO>
+
+## SEG
+## <TODO>
+
+
+# General commands
+
+## search -l
+## <TODO>
