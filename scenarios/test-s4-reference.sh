877e1cf76fc73341eddb7bd89b16b8a4c62d2d8e
PETERSBURG\Aleksandr_Sidoruk <aleksandr_sidoruk@epam.com>
2017-03-06 19:23:44 +0300

    no message

--- scenarios/test-s4-reference.sh
+++ scenarios/test-s4-reference.sh
@@ -2,6 +2,26 @@ echo
 echo Scenario 4. Reference. Will register reference genome with genes
 echo ------------------------------
 
+# Register reference
+# Register GTF with explicit name
+# Add GTF to reference
+# Check reference exists
+# Remove GTF from reference
+# Delete GTF
+# Delete reference
+
+# Register reference with explicit name
+# Register GTF with explicit name
+# Add GTF to reference
+# Remove GTF from reference
+# Delete GTF
+# Delete reference
+
+# Register reference with explicit name and GTF - single line
+# Remove GTF from reference
+# Delete GTF
+# Delete reference
+
 echo ------------------------------
 echo Scenario 4. Finished
 echo
--- scenarios/test-s5-dataset-add.sh
+++ scenarios/test-s5-dataset-add.sh
@@ -2,6 +2,39 @@ echo
 echo Scenario 5. Datasets. Will register Datasets and add files by name and paths
 echo ------------------------------
 
+echo
+echo Scenario 5. Dataset. Init reference and dataset
+echo ------------------------------
+
+# Register reference
+# Register dataset
+
+echo
+echo Scenario 5. Dataset. Add files
+echo ------------------------------
+
+# Register feature file
+# Register feature file with explicit name
+# Add feature file to dataset
+# Add feature file to dataset by explicit name
+# Add feature file to a dataset without registration
+
+# Register BAM file
+# Register BAM file with explicit name
+# Add BAM file to dataset
+# Add BAM file to dataset by explicit name
+# Add BAM file to a dataset without registration
+
+
+echo
+echo Scenario 5. Datasets. Clean-up reference, dataset and files
+echo ------------------------------
+
+# Delete feature files
+# Delete BAM files
+# Delete dataset
+# Delete reference
+
 echo ------------------------------
 echo Scenario 5. Finished
 echo
