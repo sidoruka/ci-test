449682ce5fe43b2b04b81daa84bce30fe501497d
PETERSBURG\Aleksandr_Sidoruk <aleksandr_sidoruk@epam.com>
2017-03-06 18:01:53 +0300

    no message

--- scenarios/test-s2-file-types.sh
+++ scenarios/test-s2-file-types.sh
@@ -6,33 +6,60 @@ echo
 echo Scenario 2. File types. Init reference and dataset
 echo ------------------------------
 
-# register reference with genes
-# create root dataset
+skip_it \
+  "Should configure NGB server address" \
+  "ngb set_srv http://localhost:8080/catgenome"
+
+skip_it \
+    "Should register reference with index and explicit name" \
+    "ngb reg_ref ${FASTA} --name ${REFERENCE_NAME}"
+
+skip_it \
+    "Should register gzipped genes file with explicit name" \
+    "ngb reg_file ${REFERENCE_NAME} ${GTF}?${GTF_INDEX} --name ${GTF_NAME}"
+
+skip_it \
+    "Should assign registered genes file to a reference by name" \
+    "ngb add_genes ${REFERENCE_NAME} ${GTF_NAME}"
+
+skip_it \
+    "Should register root dataset for reference" \
+    "ngb reg_dataset ${REFERENCE_NAME} ${DATASET_ROOT}"
+
+# Each file type should perform the following checks
+#   - register file
+#   - search for file
+#   - add to dataset
+#   - remove from dataset
+#   - delete file
+
+#   - register file with expl name
+#   - search for file with expl name
+#   - add to dataset by expl name
+#   - remove from dataset by expl name
+#   - delete file by expl name
+
+#   - register gzipped file with expl name
+#   - search for gzipped file with expl name 
+#   - add to dataset by expl name
+#   - remove from dataset by expl name
+#   - delete file by expl name
+
+#   - register gzipped file without index (should fail)
+#   - search for gzipped file without index (should fail)
+
+#   - register unexisting file (should fail)
+#   - search for unexisting file (should fail)
+
+#   - register file for unexisting reference (should fail)
+#   - search for file with unexisting reference (should fail)
+
+#   - register file for unexisting dataset (should fail)
+#   - search for file with unexisting dataset (should fail)
 
 echo
 echo Scenario 2. File types. BAM
 echo ------------------------------
-
-# register file
-# search for file
-# add to dataset
-# remove from dataset
-# delete file
-
-# register file with expl name
-# search for file expl name
-# add to dataset
-# remove from dataset
-# delete file
-
-# register gzipped file 
-# search for gzipped file 
-# add to dataset
-# remove from dataset
-# delete file
-
-# register gzipped file without index (should fail)
-# search for gzipped file without index (should fail)
 
 echo
 echo Scenario 2. File types. CRAM
@@ -62,8 +89,21 @@ echo
 echo Scenario 2. File types. Clean-up reference and dataset
 echo ------------------------------
 
-# register reference with genes
-# create root dataset
+it \
+    "Should delete root dataset" \
+    "ngb del_dataset ${DATASET_ROOT}"
+
+it \
+    "Should remove GTF file from reference" \
+    "ngb remove_genes ${REFERENCE_NAME}"
+
+it \
+    "Should delete GTF file" \
+    "ngb del_file ${GTF_NAME}"
+
+it \
+    "Should delete reference" \
+    "ngb del_ref ${REFERENCE_NAME}"
 
 echo ------------------------------
 echo Scenario 2. Finished
--- scenarios/test-s3-hierarchies.sh
+++ scenarios/test-s3-hierarchies.sh
@@ -1,7 +1,7 @@
 DATASETS_COUNT=3
 
 echo
-echo Scenario 3. Hierarchies operations. Will create 3-level datasets hierachy with ${DATASETS_COUNT} nodes each
+echo Scenario 3. Hierarchies operations. Will create 3-level datasets hierachy with ${DATASETS_COUNT} nodes each and check it is created
 echo ------------------------------
 
 it \
@@ -12,6 +12,12 @@ it \
     "Should register reference with index and explicit name" \
     "ngb reg_ref ${FASTA} --name ${REFERENCE_NAME}"
 
+# Build hierachy
+#   - root_dataset_N
+#       - child_dataset_N_M
+#           - child_dataset_N_M_K
+
+# Adding level 1 datasets
 for DATASET_NUM in $(seq 1 $DATASETS_COUNT)
 do
     ROOT_L1_NAME=${DATASET_ROOT}_${DATASET_NUM}
@@ -19,6 +25,7 @@ do
         "Should register root dataset ${ROOT_L1_NAME} for reference" \
         "ngb reg_dataset ${REFERENCE_NAME} ${ROOT_L1_NAME}"
 
+        # Adding level 2 datasets
         for DATASET_NUM_L2 in $(seq 1 $DATASETS_COUNT)
         do
             CHILD_L2_NAME=${DATASET_CHILD}_${DATASET_NUM}_${DATASET_NUM_L2}
@@ -26,6 +33,7 @@ do
                 "Should register child dataset ${CHILD_L2_NAME} for reference" \
                 "ngb reg_dataset ${REFERENCE_NAME} ${CHILD_L2_NAME} -p ${ROOT_L1_NAME}"
 
+                # Adding level 3 datasets
                 for DATASET_NUM_L3 in $(seq 1 $DATASETS_COUNT)
                 do
                     CHILD_L3_NAME=${CHILD_L2_NAME}_${DATASET_NUM_L3}
@@ -33,20 +41,108 @@ do
                         "Should register child dataset ${CHILD_L3_NAME} for reference" \
                         "ngb reg_dataset ${REFERENCE_NAME} ${CHILD_L3_NAME} -p ${CHILD_L2_NAME}"
                 done
+
+                # Check that ${CHILD_L2_NAME} has ${DATASETS_COUNT} child items
+                # TODO
         done
+
+        # Check that ${ROOT_L1_NAME} has ${DATASETS_COUNT} child items
+        # TODO
 done
 
+# Check that root level has ${DATASETS_COUNT} items
+# TODO
+
 echo
-echo Scenario 3. Hierarchies operations. Will check (search) created hierarchy
+echo Scenario 3. Hierarchies operations. Will delete all created datasets and check they are deleted
 echo ------------------------------
+
+# Deleting level 1 datasets
+for DATASET_NUM in $(seq 1 $DATASETS_COUNT)
+do
+    ROOT_L1_NAME=${DATASET_ROOT}_${DATASET_NUM}
+
+    # Deleting level 2 datasets
+    for DATASET_NUM_L2 in $(seq 1 $DATASETS_COUNT)
+    do
+        CHILD_L2_NAME=${DATASET_CHILD}_${DATASET_NUM}_${DATASET_NUM_L2}
+           
+        # Deleting level 3 datasets
+        for DATASET_NUM_L3 in $(seq 1 $DATASETS_COUNT)
+        do
+            CHILD_L3_NAME=${CHILD_L2_NAME}_${DATASET_NUM_L3}
+            it \
+                "Should delete L3 child dataset ${CHILD_L3_NAME}" \
+                "ngb del_dataset ${CHILD_L3_NAME}"
+            done
+
+            # Check that ${CHILD_L2_NAME} has no child items
+            # TODO
+
+            it \
+                "Should delete L2 child dataset ${CHILD_L2_NAME}" \
+                "ngb del_dataset ${CHILD_L2_NAME}"
+        done
+
+        # Check that ${ROOT_L1_NAME} has no child items
+        # TODO
+
+        it \
+            "Should delete root dataset ${ROOT_L1_NAME}" \
+            "ngb del_dataset ${ROOT_L1_NAME}"
+done
+
+# Check that no root items left
+# TODO
 
 echo
 echo Scenario 3. Hierarchies operations. Will move child nodes between root nodes
 echo ------------------------------
 
+it \
+    "Should register first root dataset ${DATASET_ROOT}_1" \
+    "ngb reg_dataset ${REFERENCE_NAME} ${DATASET_ROOT}_1"
+
+it \
+    "Should register dataset ${DATASET_CHILD} as a child to ${DATASET_ROOT}_1" \
+    "ngb reg_dataset ${REFERENCE_NAME} ${DATASET_CHILD} -p ${DATASET_ROOT}_1"
+
+it \
+    "Should register second root dataset ${DATASET_ROOT}_2" \
+    "ngb reg_dataset ${REFERENCE_NAME} ${DATASET_ROOT}_2"
+
+# Check datasets are created
+# TODO
+
+it \
+    "Should move ${DATASET_CHILD} from ${DATASET_ROOT}_1 to ${DATASET_ROOT}_2" \
+    "ngb move_dataset ${DATASET_CHILD} ${DATASET_ROOT}_2"
+
+# Check dataset is moved created
+# TODO
+
+# Clean datasets
+it \
+    "Should delete child dataset ${DATASET_CHILD}" \
+    "ngb del_dataset ${DATASET_CHILD}"
+
+it \
+    "Should delete root dataset ${DATASET_ROOT}_1" \
+    "ngb del_dataset ${DATASET_ROOT}_1"
+
+it \
+    "Should delete root dataset ${DATASET_ROOT}_2" \
+    "ngb del_dataset ${DATASET_ROOT}_2"
+
+
 echo
-echo Scenario 3. Hierarchies operations. Will delete all created datasets
+echo Scenario 3. Hierarchies operations. Clean-up reference
 echo ------------------------------
+
+it \
+    "Should delete reference" \
+    "ngb del_ref ${REFERENCE_NAME}"
+
 
 echo ------------------------------
 echo Scenario 3. Finished
